092f345cf752dcbcc28eaba448592f21053c8345
Majoros-Peter <70958388+Majoros-Peter@users.noreply.github.com>
2022-03-26 10:59:10 +0100

    Update Kockapóker.py
    by: Majoros-Peter
    
    made the code shorter

--- Kockapóker.py
+++ Kockapóker.py
@@ -3,13 +3,12 @@ from tkinter import *
 
 root = Tk()
 root.title("Kockapóker")  # cím
-width= root.winfo_screenwidth()
-height= root.winfo_screenheight()
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
 root.geometry("%dx%d" % (width, height))
 root.configure(bg="white")  # háttérszín
 
-dobasok = []  # kockákat tárolja
-bal_dobasok = []  # bot kockáit tárolja
+dobasok, bal_dobasok = [], []  # kockákat tárolja, a másik meg a bot kockáit
 
 
 # "dob" a kockákkal
@@ -27,8 +26,7 @@ def szemet():
     szum = 0
     for kocka in dobasok:
         szum += kocka
-    button_1['state'] = DISABLED
-    button_1['text'] = szum
+    button_1['state'], button_1['text'] = DISABLED, szum
     bal_oldal()
 
 
@@ -55,8 +53,7 @@ def ket_par():
         szum = parok[-1] * 2 + parok[-3] * 2
     except:
         szum = 0
-    button_3['text'] = szum
-    button_3['state'] = DISABLED
+    button_3['text'], button_3['state'] = szum, DISABLED
     bal_oldal()
 
 
@@ -73,8 +70,7 @@ def drill():
 
 # full house
 def full():
-    par = 0
-    drill = 0
+    par, drill = 0, 0
     for sorszam in dobasok:
         if dobasok.count(sorszam) >= 3:
             drill = sorszam * 3
@@ -95,8 +91,7 @@ def full():
 
 # kis sor
 def kis_sor():
-    seged = 1
-    kis_sor = True
+    seged, kis_sor = 1, True
     while seged != 6:
         if dobasok.count(seged) == 0:
             kis_sor = False
@@ -111,8 +106,7 @@ def kis_sor():
 
 # nagy sor
 def nagy_sor():
-    seged = 2
-    nagy_sor = True
+    seged, nagy_sor = 2, True
     while seged != 7:
         if dobasok.count(seged) == 0:
             nagy_sor = False
@@ -148,24 +142,19 @@ def nagy_poker():
 
 
 def be_ir(button, ertek):
-    button['text'] = ertek
-    button['state'] = DISABLED
+    button['text'], button['state'] = ertek, DISABLED
 
 
 def kilep():
     root.destroy()
 
 
-def ujra():
-    pass
+def ujra(gomb_lista, ellenseg_gombok, szoveg):
+    for (gomb, gomb2, szo) in zip(gomb_lista, ellenseg_gombok, szoveg): gomb['state'], gomb2['state'], gomb['text'], gomb2['text'] = NORMAL, NORMAL, szo, szo
 
 
 def bal_oldal():
-    egyik_par = 0
-    ertekek = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    k_sor = [1, 2, 3, 4, 5]
-    n_sor = [2, 3, 4, 5, 6]
-    lista = dobas(bal_dobasok, bal_label_dobasok)
+    egyik_par, ertekek, k_sor, n_sor, lista = 0, [0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 2, 3, 4, 5], [2, 3, 4, 5, 6], dobas(bal_dobasok, bal_label_dobasok)
 
     for szam in lista:
         if bal_button_1['state'] != DISABLED:   # szemét
@@ -210,24 +199,11 @@ def bal_oldal():
             if gombok[index]['state'] != DISABLED:
                 be_ir(gombok[index], ertekek[index])
                 break
-    elif ertekek.index(max(ertekek)) == 0:
-        be_ir(gombok[0], ertekek[0])
-    elif ertekek.index(max(ertekek)) == 1:
-        be_ir(gombok[1], ertekek[1])
-    elif ertekek.index(max(ertekek)) == 2:
-        be_ir(gombok[2], ertekek[2])
-    elif ertekek.index(max(ertekek)) == 3:
-        be_ir(gombok[3], ertekek[3])
-    elif ertekek.index(max(ertekek)) == 4:
-        be_ir(gombok[4], ertekek[4])
-    elif ertekek.index(max(ertekek)) == 5:
-        be_ir(gombok[5], ertekek[5])
-    elif ertekek.index(max(ertekek)) == 6:
-        be_ir(gombok[6], ertekek[6])
-    elif ertekek.index(max(ertekek)) == 7:
-        be_ir(gombok[7], ertekek[7])
-    elif ertekek.index(max(ertekek)) == 8:
-        be_ir(gombok[8], ertekek[8])
+    else:
+        for index in range(9):
+            if ertekek.index(max(ertekek)) == index:
+                be_ir(gombok[index], ertekek[index])
+                break
 
 
 label_dobasok = Label(root, width=10, padx=40, pady=20, bg="gray", fg="white", borderwidth=4, relief="sunken")
@@ -241,6 +217,7 @@ button_7 = Button(root, text="Nagy sor", width=10, padx=40, pady=20, bg="gray", 
 button_8 = Button(root, text="Kis póker", width=10, padx=40, pady=20, bg="gray", fg="yellow", command=lambda: [kis_poker(), dobas(dobasok, label_dobasok)])
 button_9 = Button(root, text="Nagy póker", width=10, padx=40, pady=20, bg="gray", fg="yellow", command=lambda: [nagy_poker(), dobas(dobasok, label_dobasok)])
 button_kilep = Button(root, text="Kilépés", width=10, padx=40, pady=20, bg="gray", fg="yellow", command=kilep)
+button_ujra = Button(root, text="Újrakezdés", width=10, padx=40, pady=20, bg="gray", fg="yellow", command=lambda: ujra(jatekos_gombok, gombok, szovegek))
 
 space = Label(root, width=230, bg="white", fg="white")
 
@@ -266,6 +243,7 @@ button_7.grid(row=7, column=0)
 button_8.grid(row=8, column=0)
 button_9.grid(row=9, column=0)
 button_kilep.grid(row=10, column=0)
+button_ujra.grid(row=11, column=0)
 space.grid(row=0, column=1)
 bal_label_dobasok.grid(row=0, column=2)
 bal_button_1.grid(row=1, column=2)
@@ -278,7 +256,7 @@ bal_button_7.grid(row=7, column=2)
 bal_button_8.grid(row=8, column=2)
 bal_button_9.grid(row=9, column=2)
 
-gombok = [bal_button_1, bal_button_2, bal_button_3, bal_button_4, bal_button_5, bal_button_6, bal_button_7, bal_button_8, bal_button_9]
+gombok, jatekos_gombok, szovegek = [bal_button_1, bal_button_2, bal_button_3, bal_button_4, bal_button_5, bal_button_6, bal_button_7, bal_button_8, bal_button_9], [button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9], ["Szemét", "Pár", "Két pár", "Drill", "Full", "Kis sor", "Nagy sor", "Kis póker", "Nagy póker"]
 
 dobas(dobasok, label_dobasok)
 root.mainloop()
